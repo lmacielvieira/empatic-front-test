61b25b5aaa7c17ea1070ac990913dd38feca0573
Lucas Maciel Vieira <lucas@Lucass-MacBook-Pro.local>
2020-12-18 12:54:19 -0300

    [LandingPage] added embrace section and reformulated responsiviness of images and sections

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Col} from 'antd'
 import './style.less'
-import {SETTINGS} from '../../settings'
+import {IMAGES, SETTINGS} from '../../settings'
 
 export default class LandingPage extends React.Component {
 	_pageName = 'landing-page'
@@ -31,25 +33,74 @@ export default class LandingPage extends React.Component {
 	// Rendering
 	// -------------------------------------------------------------------------//
 
+	renderEmbraceBtn = (className: string) => {
+		return (
+			<Link className={className} to="#">
+				{SETTINGS.LandingPage.embraceBtn}
+			</Link>
+		)
+	}
+
 	renderSmartSection = () => {
 		return (
 			<section className={`${this._pageName}-smart-section`}>
-				<span className={`${this._pageName}-title defaultTitle`}>
-					{SETTINGS.LandingPage.smartTitle}
-				</span>
-				<div className={`${this._pageName}-smart-btn`}>
-					{SETTINGS.LandingPage.smartBtnLabel}
+				<div className={`${this._pageName}-container`}>
+					<Col xs={24} md={12} className={`${this._pageName}-wrapper`}>
+						<span className={`${this._pageName}-title defaultTitle`}>
+							{SETTINGS.LandingPage.smartTitle}
+						</span>
+						<Link className={`${this._pageName}-smart-btn`} to="#">
+							{SETTINGS.LandingPage.smartBtnLabel}
+						</Link>
+						<p className={`${this._pageName}-label`}>
+							{SETTINGS.LandingPage.smartLabel}
+						</p>
+					</Col>
+					<Col xs={0} md={12} className={`${this._pageName}-wrapper`} />
 				</div>
-				<p className={`${this._pageName}-label`}>
-					{SETTINGS.LandingPage.smartLabel}
-				</p>
+			</section>
+		)
+	}
+
+	renderEmbraceSection = () => {
+		return (
+			<section className={`${this._pageName}-embraced-section`}>
+				<div className={`${this._pageName}-container`}>
+					<Col xs={24} md={12} className={`${this._pageName}-wrapper`}>
+						<div className={`${this._pageName}-embrace-logo`} />
+						<span className={`${this._pageName}-title defaultTitle`}>
+							{SETTINGS.LandingPage.companionLabel}
+						</span>
+						<p className={`${this._pageName}-label`}>
+							{SETTINGS.LandingPage.embraceDesc}
+						</p>
+						{this.renderEmbraceBtn(`${this._pageName}-embrace-btn big-sc-btn`)}
+					</Col>
+					<Col
+						xs={24}
+						md={12}
+						className={`${this._pageName}-wrapper`}
+						style={{alignItems: 'center'}}>
+						<img
+							className={`${this._pageName}-embrace-img`}
+							src={IMAGES.embrace}
+							alt=""
+						/>
+						{this.renderEmbraceBtn(
+							`${this._pageName}-embrace-btn small-sc-btn`
+						)}
+					</Col>
+				</div>
 			</section>
 		)
 	}
 
 	render() {
 		return (
-			<div className={`${this._pageName}`}>{this.renderSmartSection()}</div>
+			<div className={`${this._pageName}`}>
+				{this.renderSmartSection()}
+				{this.renderEmbraceSection()}
+			</div>
 		)
 	}
 }
--- src/settings/images.tsx
+++ src/settings/images.tsx
@@ -1,7 +1,9 @@
 // icons
 import search from '../assets/images/icons/ic-search.png'
+import embrace from '../assets/images/landing/embrace_front-xhdpi.jpg'
 
 export const IMAGES = {
+	embrace,
 	icon: {
 		search
 	}
